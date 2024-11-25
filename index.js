1d43b1421a2c2d88b9cb278e6accecc2c9da5a8d
vrbprog <46716068+vrbprog@users.noreply.github.com>
2024-11-25 13:21:22 +0000

    Deploying to gh-pages from @ vrbprog/portfolio@453d3434fc63a3c621c2fe7f94299e98dbea1ce6 🚀

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ import{a as D,i as M,S as _,N as E,K as T,c as P}from"./assets/vendor-D5dh7xdJ.j
               <span>VISIT</span>
               <svg width="24" height="24">
                 <use
-                  <use href="../img/icons.svg#icon-arrow-right"></use>
+                  <use href="./img/icons.svg#icon-arrow-right"></use>
                 ></use>
               </svg>
             </div>
