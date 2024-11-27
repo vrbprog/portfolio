f29869a5b2181b9b95c5a792a7306a5d1cb6be55
Roman <vrbox@i.ua>
2024-11-27 12:04:02 +0200

    button_icon

--- src/js/projects.js
+++ src/js/projects.js
@@ -251,8 +251,8 @@ function createMarkupProjects(images) {
             target="blanc"
           >
             VISIT
-            <svg width="24" height="24">
-              <use href="../img/my_projects/icon-icon_visit.svg#icon-icon_visit"></use>
+            <svg class="button_icon" width="24" height="24">
+              <use href="../img/icons.svg#icon-arrow45"></use>
             </svg>
           </a>
         </div>
